d5b710462c09a48df23c6b609858371fbee986a5
Vincent Horvath <vhorvath2010@gmail.com>
2021-01-04 10:59:33 -0500

    Initial commit

---
+++ src/main/java/io/github/vhorvath2010/airidalechestshops/AiridaleChestShops.java
@@ -0,0 +1,11 @@
+package io.github.vhorvath2010.airidalechestshops;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class AiridaleChestShops extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        saveDefaultConfig();
+    }
+}
